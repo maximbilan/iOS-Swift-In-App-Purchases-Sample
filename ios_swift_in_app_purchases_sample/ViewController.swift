19a3ac3775dcd580af34aeccffbb43cf7d3efad7
Maxim Bilan <maximb.mail@gmail.com>
2015-07-27 13:15:45 +0300

    Added project sample

---
+++ ios_swift_in_app_purchases_sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios_swift_in_app_purchases_sample
+//
+//  Created by Maxim Bilan on 7/27/15.
+//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+

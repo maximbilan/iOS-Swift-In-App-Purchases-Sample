5a475af1f8fc725c92d7be8905e4a58c4d74803a
Maxim Bilan <maximb.mail@gmail.com>
2015-07-27 13:20:55 +0300

    Added InAppPurchase class

---
+++ ios_swift_in_app_purchases_sample/InAppPurchase.swift
@@ -0,0 +1,9 @@
+//
+//  InAppPurchase.swift
+//  ios_swift_in_app_purchases_sample
+//
+//  Created by Maxim Bilan on 7/27/15.
+//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
+//
+
+import Foundation
